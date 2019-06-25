09ab9096be2c566eb14a8eb7663793f33f995133
Paul Ritter <ritter.paul1@googlemail.com>
2019-06-25 21:43:20 +0200

    get and get requests, also issues/prs

--- Modules/Github/GetIssue.cs
+++ Modules/Github/GetIssue.cs
@@ -5,5 +5,22 @@ namespace Crab.Modules
 {
     public class IssueModule : ModuleBase<SocketCommandContext>
     {
+        [Command("issue")]
+        public Task getIssue(string issueid){
+            return getIssue(issueid, "");
+        }
+
+        [Command("issue")]
+        public Task getIssue(string issueid, string prefix){
+            string repo = Utils.get_repo(prefix);
+            if(repo == null)
+                return ReplyAsync("Invalid prefix");
+
+            dynamic obj = Utils.get_json(Utils.github_url($"/repos/{repo}/issues/{issueid}"));
+            if(obj == null)
+                return ReplyAsync("Issue not found");
+
+            return ReplyAsync(embed: Utils.embed_issue(obj, repo));
+        }
     }
 }
--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -1,5 +1,8 @@
 using System.IO;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Net;
+using Discord;
 
 namespace Crab{
     static class Utils {
@@ -59,6 +62,93 @@ namespace Crab{
 
         public static string github_url(string sub){
             return $"https://api.github.com{sub}";
+        }
+
+        public static string get_request(string uri){
+            return get_request(uri, "");
+        }
+    
+        public static string get_request(string uri, string accept)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            IConfiguration config = GetConfig();
+            request.Headers["Authorization"] = config["git_auth"];
+            request.UserAgent = "Crab (@PaulRitter)";
+            if(accept != ""){
+                request.Accept = accept;
+            }else{
+                request.Accept = "application/vnd.github.v3+json";
+            }
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using(Stream stream = response.GetResponseStream())
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static dynamic get_json(string uri){
+            return get_json(uri, "");
+        }
+
+        public static dynamic get_json(string uri, string accept){
+            return JsonConvert.DeserializeObject(get_request(uri, accept));
+        }
+
+        public static string get_repo(string prefix){
+            IConfiguration config = GetConfig();
+            foreach (IConfigurationSection repo in config.GetSection("repos").GetChildren()){
+                if(repo.GetValue<string>("prefix") == prefix) return repo.GetValue<string>("repo");
+            }
+            return null;
+        }
+
+        public static string format_desc(string desc){
+            string res = desc; //remove html comments
+            if(res.Length > 500)
+                res = res.Substring(0, 500)+"...";
+            return res;
+        }
+
+        public static Embed embed_issue(dynamic issue, string repo){
+            string title = issue.title;
+            string desc = issue.body;
+            string avatar_url = issue.avatar_url;
+
+            dynamic prcontent = null;
+            if(issue.pull_request != null){
+                prcontent = get_json(github_url($"/repos/{repo}/pulls/{issue.number}"));
+            }
+
+            EmbedBuilder embed = new EmbedBuilder();
+            string emoji = "";
+            if(issue.state == "open"){
+                if(prcontent != null){
+                    emoji = "<:PRopened:245910125041287168>";
+                }else{
+                    emoji = "<:ISSopened:246037149873340416>";
+                }
+                embed.WithColor(0x6CC644);
+            }else if(prcontent != null){
+                if(prcontent.merged){
+                    emoji = "<:PRmerged:437316952772444170>";
+                    embed.WithColor(0x6E5494);
+                }else{
+                    emoji = "<:PRclosed:246037149839917056>";
+                    embed.WithColor(0xFF4444);
+                }
+            }else{
+                emoji = "<:ISSclosed:246037286322569216>";
+                embed.WithColor(0xFF4444);
+            }
+
+            embed.WithTitle(emoji + $"{issue.title}")
+                .WithDescription(format_desc($"{issue.body}"))
+                .AddField("Checks", "todo: success\ntodo: failure\ntodo: neutral")
+                .WithFooter($"{repo}#{issue.number} by {issue.user.login}",$"{issue.avatar_url}");
+            return embed.Build();
         }
     }
 }
